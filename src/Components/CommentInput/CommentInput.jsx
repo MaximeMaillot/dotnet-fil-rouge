6fec6d00e3f8c92fd4c31eb1efea143009f35f8e
Maxime Maillot <maillot.maxime62@gmail.com>
2023-04-27 16:55:39 +0200

    add taskLightbox

---
+++ src/Components/CommentInput/CommentInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CommentInput.css';
+
+const CommentInput = () => {
+    return (
+        <div>
+            <input type='text' placeholder='Ecrivez un commentaire'></input>
+        </div>
+    );
+};
+
+export default CommentInput;
--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./Task.css";
+import TaskBox from '../TaskBox/TaskBox';
+import { useState } from 'react';
 
 const Task = ({task}) => {
+    const [taskBoxDisplay, setTaskBoxDisplay] = useState(false)
     return (
-        <div className='Task'>
-            {task.name}
+        <div className='Task' onClick={() => {setTaskBoxDisplay(!taskBoxDisplay)}}>
+            <div>{task.name}</div>
+            <div>{task.description}</div>
+            {taskBoxDisplay ? <TaskBox task={task}/> : undefined}
         </div>
     );
 };
