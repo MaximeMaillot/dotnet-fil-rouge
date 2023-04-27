31730fd4640ac54cdca89d07053bead17d92cf6b
Maxime Maillot <maillot.maxime62@gmail.com>
2023-04-27 10:23:39 +0200

    add skeleton to project and task component

---
+++ src/Components/Project/Project.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Project.css";
+
+const Project = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Project;
---
+++ src/Components/Task/Task.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Task.css";
+
+const Task = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Task;
