a6bd30f11b4b213c1c82ae18bb40890197bcbc43
Maxime Maillot <maillot.maxime62@gmail.com>
2023-04-28 11:58:07 +0200

    add accueil view

---
+++ src/Views/AccueilView/AccueilView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./AccueilView.css";
+
+const AccueilView = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AccueilView;
