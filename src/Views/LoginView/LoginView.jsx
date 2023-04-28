a0c7f907a7cfa27e20316e7361f7e640a7db2027
avril <avril.chai.demond@gmail.com>
2023-04-28 11:12:13 +0200

    login js

--- src/Views/LoginView/LoginView.jsx
+++ src/Views/LoginView/LoginView.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import "./LoginView.css";
+import members from "./../../data/test-members.json";
+members = members.members;
 
+
+
+function connection() {
+    let loginName = document.querySelector('#loginName');
+    let motDePasse = document.querySelector('#motDePasse');
+    if (loginName.value == "" || motDePasse.value == "") {
+        console.log("saisir")
+    } else {
+        for (let i = 0; i < members.length; i++) {
+            if (loginName.value == members[i].name) {
+                if (motDePasse.value == members[i].password) {
+                    console.log("correct")
+                    return;
+                }
+                else {
+                    console.log("wrong password");
+                }
+            }
+        }
+    }
+}
 
 
 const LoginView = () => {
@@ -8,13 +31,13 @@ const LoginView = () => {
         <div>
             <div className="wraperCreatecompte">
                 <div className="createAccount">
-                <p className='para1'>Créez votre compte</p>
+                    <p className='para1'>Créez votre compte</p>
                     <h1>Trello</h1>
                     <p className='para2'>Votre login</p>
-                    <input type="text" />
+                    <input id='loginName' type="text" />
                     <p className='para3'>Votre mot de passe</p>
-                    <input type="text" />
-                    <button className='commencer'>Créer mon compte</button>
+                    <input id='motDePasse' type="text" />
+                    <button onClick={() => { connection() }} className='commencer'>Créer mon compte</button>
                 </div>
             </div>
         </div>
