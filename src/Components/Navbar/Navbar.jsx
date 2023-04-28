aefe03eabf96b68cd9d7903fcc4b4d2e121c2e41
fred <yetinessy@gmail.com>
2023-04-28 16:51:22 +0200

    avancée accueil

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import ProjectsView from '../../Views/ProjectsView/ProjectsView';
 import LoginView from '../../Views/LoginView/LoginView';
 import SignupView from '../../Views/SignupView/SignupView';
 import MembersView from '../../Views/MembersView/MembersView';
+import AccueilView from '../../Views/AccueilView/AccueilView';
 import dataProjects from "../../data/test-projects.json";
 import { useState } from 'react';
 
@@ -22,28 +23,35 @@ const Navbar = () => {
     const [projects, setProjects] = useState(dataProjects.projects);
     const id = localStorage.getItem("currentProjectId")
     const [currentProject, setCurrentProject] = useState(getProjectById(projects, id));
+    const [currentUser, setCurrentUser] = useState('maxime');
 
     return (
         <div >
             <BrowserRouter>
                 <div className='Navbar'>
-                        <Link to="/home"><img src="/logo-trallo.jpg" alt="Logo Trallo" className='logo'/></Link>
+                    <Link to="/"><img src="/logo-trallo-perso.png" alt="Logo Trallo" className='logo' /></Link>
                     <div className='bouton1'>
                         <Link to="/projects">Espaces de travail </Link>
                     </div>
-                    <div className='bouton2'>
-                        <Link to="/login">Se connecter</Link>
-                    </div>
-                    <div className='bouton3'>
-                        <Link to="/signup">Inscription</Link>
-                    </div>
+                    {currentUser ?  <div className='membre'>
+                                        <Link to="/Memberprofil">M</Link>
+                                    </div> :
+                        <>
+                        <div className='bouton2'>
+                            <Link to="/login">Se connecter</Link>
+                        </div>
+                            <div className='bouton3'>
+                                <Link to="/signup">Inscription</Link>
+                            </div>
+                        </>
+                        }
                 </div>
                 <Routes>
                     <Route path='/projects' element={<ProjectsView projects={projects} setProjects={setProjects} currentProject={currentProject} setCurrentProject={setCurrentProject} />}></Route>
                     <Route path='/members' element={<MembersView />}></Route>
                     <Route path='/login' element={<LoginView />}></Route>
                     <Route path='/signup' element={<SignupView />}></Route>
-                    <Route path='/*' element={<ProjectsView />}></Route>
+                    <Route path='/*' element={<AccueilView />}></Route>
                 </Routes>
                 <div>
                     <Outlet />
