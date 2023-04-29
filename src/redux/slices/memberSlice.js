4cf184c713f7cfdb4a3a874d47f7b52b348c5887
MaximeMaillot <maillot.maxime62@gmail.com>
2023-04-29 22:03:20 +0200

    add member reducer

--- src/redux/slices/memberSlice.js
+++ src/redux/slices/memberSlice.js
@@ -3,7 +3,10 @@ import membersData from './../../data/test-members.json';
 
 export const memberSlice = createSlice({
     name: "member",
-    initialState: membersData.members,
+    initialState: {
+        members: membersData.members,
+        currentMemberId: -1
+    },
     reducers: {
         addMember: (state, action) => {
             return [...state, action.payload]
@@ -12,10 +15,23 @@ export const memberSlice = createSlice({
             return state.filter((s) => {
                 return s.member_id !== action.payload.id
             })
+        },
+        connectMember: (state, action) => {
+            const member = state.members.find((member) => member.username == action.payload.username && member.password == action.payload.password)
+            if (member) {
+                state.currentMemberId = member.member_id
+            } else {
+                state.currentMemberId = -1
+            }
+            return state
+        },
+        disconnectMember: (state, action) => {
+            state.currentMemberId = -1
+            return state
         }
     }
 })
 
-export const { addMember, removeMember } = memberSlice.actions;
+export const { addMember, removeMember, connectMember, disconnectMember } = memberSlice.actions;
 
 export default memberSlice.reducer;
